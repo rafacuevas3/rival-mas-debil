5a02363f528f951a6026d878c6613f49d8e92cce
Rafa Cuevas <rafacuevas3@gmail.com>
2015-04-25 18:55:40 -0500

    Reparado

--- resources/views/game/final.blade.php
+++ resources/views/game/final.blade.php
@@ -4,8 +4,76 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-8 col-md-offset-2">
+							<h4 class="text-center">Puntos acumulados:</h4>
+							<h4 class="text-warning text-center"><strong>{{ $acum }}</strong></h4>
+							<img src="{{ asset('/images/background.jpg') }}" alt="FLISoL" class="img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<h3 class="text-center text-success">Respuestas correctas:</h3>
+						<div class="col-md-8 col-md-offset-2">
+							<h2 class="text-info">Jugador 1: <strong id="respuestas_jugador1" class="text-success">0</strong></h2>
+							<h2 class="text-info">Jugador 2: <strong id="respuestas_jugador2" class="text-success">0</strong></h2>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 		<div class="col-md-9">
+			<h2 id="titulo_ronda" class="text-center">Ronda Final</h2>
+			<br><br>
+			<div class="row">
+				<div class="col-md-2">
+					<h4 class="text-info text-center">Jugador 1:</h4>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j1_1" class="pregunta btn btn-block btn-warning">1</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j1_2" class="pregunta btn btn-block btn-default disabled">2</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j1_3" class="pregunta btn btn-block btn-default disabled">3</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j1_4" class="pregunta btn btn-block btn-default disabled">4</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j1_5" class="pregunta btn btn-block btn-default disabled">5</a>
+				</div>
+			</div>
+			<br><br>
+			<div class="row">
+				<div class="col-md-2">
+					<h4 class="text-info text-center">Jugador 2:</h4>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j2_1" class="pregunta btn btn-block btn-default disabled">1</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j2_2" class="pregunta btn btn-block btn-default disabled">2</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j2_3" class="pregunta btn btn-block btn-default disabled">3</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j2_4" class="pregunta btn btn-block btn-default disabled">4</a>
+				</div>
+				<div class="col-md-2">
+					<a href="#" id="j2_5" class="pregunta btn btn-block btn-default disabled">5</a>
+				</div>
+			</div>
+			<br><br>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<a href="#" id="correcto" class="btn btn-success">Correcto</a>
+					<a href="#" id="incorrecto" class="btn btn-danger">Incorrecto</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -23,18 +91,21 @@
 	var muerte_subita = false;
 	
 	$(document).on('click', '#correcto', function(){
-		if( !muerte_subita )
-			siguiente(true);
-		else
-			compara(true);
+		x_s(true);
 	});
 
 	$(document).on('click', '#incorrecto', function(){
-		if( !muerte_subita )
-			siguiente(false);
-		else
-			compara(false);
+		x_s(false);
 	});
+
+	function x_s(band){
+		if( !muerte_subita ){
+			siguiente(band);
+		}
+		else{
+			compara(band);
+		}
+	}
 
 	function siguiente(band){
 		if( band )
